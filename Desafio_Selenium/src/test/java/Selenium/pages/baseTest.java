c9563365c2885239d99e447b70e40b704b3571ad
Rocha Jorge <djorger@minsait.com>
2026-06-03 00:40:26 -0300

    Test Dashboard

--- Desafio_Selenium/src/test/java/Selenium/pages/baseTest.java
+++ Desafio_Selenium/src/test/java/Selenium/pages/baseTest.java
@@ -8,12 +8,10 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class baseTest {
     private final String URL = "file:///C:/Users/djorger/Desktop/Automacao/desafio_final_selenium_java.html";
     protected WebDriver driver;
-    //protected LoginPage loginPage;
 
     @Before
     public final void iniciar() {
         driver = new FirefoxDriver();
-        //loginPage = new LoginPage(driver);
         driver.get(URL);
     }
 
